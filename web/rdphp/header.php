a651cb1d5bd4315c89721e710f06ce42038ceae7
Brian P. McGlynn <brian.mcglynn@geneseemedia.net>
2019-08-03 18:17:09 -0400

    Initial Check-In

---
+++ web/rdphp/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo $title; ?></title>
+<?php
+  //Insert JS Scripts
+
+  foreach($js as $script){
+
+?>
+    <script src="js/<?php echo $script; ?>" type="text/javascript"></script>
+<?php } ?>
+    <script src="js/jquery-2.2.2.min.js" type="text/javascript"></script>
+    <link href="css/<?php echo $css; ?>" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <div id="content">
